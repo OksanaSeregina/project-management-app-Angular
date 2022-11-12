f276503f6fffcb5520c66b66b56bc04ca81f1c2b
Aliaksandr Siarohin <a.siarohin@gmail.com>
2022-11-12 22:21:56 +0600

    Revert "Ivan main"

--- project-management-app/src/app/modules/main/state/actions.ts
+++
@@ -1,9 +0,0 @@
-import { createAction, props } from '@ngrx/store';
-
-import { Border } from '../models/models';
-
-const borderActionSourse = '[userReducer]';
-
-export const addBorder = createAction(`${borderActionSourse} Add Border`, props<{ cardBorder: Border }>());
-
-export const removeBorder = createAction(`${borderActionSourse} Remove Border`, props<{ titleBorder: string }>());
