5e897f7c76985f72580a093b6e9c6d432a14812a
OksanaSeregina <85890740+OksanaSeregina@users.noreply.github.com>
2022-11-19 22:04:04 +0600

    fix: update spinner

--- project-management-app/src/app/modules/shared/components/spinner/spinner.service.ts
+++ project-management-app/src/app/modules/shared/components/spinner/spinner.service.ts
@@ -1,7 +1,6 @@
-import { DOCUMENT } from '@angular/common';
-import { Inject, Injectable, OnDestroy } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
 import { BehaviorSubject, Observable, Subject } from 'rxjs';
-import { publishReplay, refCount, debounceTime, takeUntil, tap } from 'rxjs/operators';
+import { publishReplay, refCount, debounceTime, takeUntil } from 'rxjs/operators';
 
 @Injectable()
 export class SpinnerService implements OnDestroy {
@@ -10,15 +9,8 @@ export class SpinnerService implements OnDestroy {
 
   public isVisible$: Observable<boolean>;
 
-  constructor(@Inject(DOCUMENT) private document: Document) {
+  constructor() {
     this.isVisible$ = this.visibleSubj.asObservable().pipe(
-      tap((isVisible) => {
-        if (isVisible) {
-          this.document.body.classList.add('no-scroll');
-        } else {
-          this.document.body.classList.remove('no-scroll');
-        }
-      }),
       // debounceTime using to fix error ExpressionChangedAfterItHasBeenCheckedError
       debounceTime(100),
       takeUntil(this.destroySubj),
