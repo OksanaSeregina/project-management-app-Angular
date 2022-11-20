9ecca15c3c0b476698275b2089cecc7a412e1db1
OksanaSeregina <85890740+OksanaSeregina@users.noreply.github.com>
2022-11-20 14:23:50 +0600

    feat: create http error interceptor

--- project-management-app/src/app/core/interceptors/interceptors.module.ts
+++ project-management-app/src/app/core/interceptors/interceptors.module.ts
@@ -1,12 +1,14 @@
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { AuthInterceptor } from './auth.interceptor';
+import { HttpErrorInterceptor } from './http-error.interceptor';
 import { LoaderInterceptor } from './loader.interceptor';
 
 @NgModule({
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: LoaderInterceptor, multi: true },
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: HttpErrorInterceptor, multi: true },
   ],
 })
 export class InterceptorsModule {}
