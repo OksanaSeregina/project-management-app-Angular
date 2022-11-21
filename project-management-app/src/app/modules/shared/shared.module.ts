c2609353f14da931900bc85c21e37304052fbec5
OksanaSeregina <85890740+OksanaSeregina@users.noreply.github.com>
2022-11-21 13:19:43 +0600

    feat: make logo clickable

--- project-management-app/src/app/modules/shared/shared.module.ts
+++ project-management-app/src/app/modules/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 import {
   BoardModalComponent,
@@ -22,7 +23,7 @@ import { PipesModule } from './pipes';
     ButtonMulticolorComponent,
     BoardModalComponent,
   ],
-  imports: [CommonModule, MaterialModule, TranslateModule, PipesModule, FormsModule],
+  imports: [CommonModule, RouterModule, MaterialModule, TranslateModule, PipesModule, FormsModule],
   exports: [
     HeaderComponent,
     FooterComponent,
